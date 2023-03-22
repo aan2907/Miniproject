797c0163a5c2bfb38fa33c5aa9e7b7bdef684567
Ahad Anzar <88426463+ahadanzar@users.noreply.github.com>
2023-03-22 02:49:49 -0700

    Add files via upload

---
+++ dev/validate/validate.py
@@ -0,0 +1,14 @@
+import re
+
+def valid(reading):
+  regcodes=['ap', 'ar', 'as', 'br', 'cg', 'ga', 'gj', 'hr', 'hp', 'jk', 'jh', 'ka', 'kl', 'mp', 'mh', 'mn', 'ml', 'mz', 'nl', 'od', 'pb', 'rj', 'sk', 'tn', 'tr', 'up', 'uk', 'wb', 'ts', 'an', 'ch', 'dn', 'dd', 'ld', 'dl', 'py']
+  for i in regcodes:
+    plate = i + r'\s*\d{2}\s*[a-z]{2}\s*\d{4}'
+    plate=re.compile(plate)
+    matchfound=re.fullmatch(plate, reading.lower())
+    if matchfound:
+      return matchfound
+
+test = "py34 nh 3209"
+
+print(valid(test))
