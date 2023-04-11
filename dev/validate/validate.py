7fa6e378483823bac4b72abbc4020b93580f4570
Ahad Anzar <88426463+ahadanzar@users.noreply.github.com>
2023-04-11 21:19:11 +0530

    Update validate.py

--- dev/validate/validate.py
+++ dev/validate/validate.py
@@ -3,7 +3,7 @@ import re
 def valid(reading):
   regcodes=['ap', 'ar', 'as', 'br', 'cg', 'ga', 'gj', 'hr', 'hp', 'jk', 'jh', 'ka', 'kl', 'mp', 'mh', 'mn', 'ml', 'mz', 'nl', 'od', 'pb', 'rj', 'sk', 'tn', 'tr', 'up', 'uk', 'wb', 'ts', 'an', 'ch', 'dn', 'dd', 'ld', 'dl', 'py']
   for i in regcodes:
-    plate = i + r'\s*\d{2}\s*[a-z]{2}\s*\d{1-4}'
+    plate = i + r'\s*\d{2}\s*[a-z]{1-2}\s*\d{1-4}'
     plate=re.compile(plate)
     matchfound=re.fullmatch(plate, reading.lower())
     if matchfound:
