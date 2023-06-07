24ad9690e5282522a395cbdb845635e3b75c7d6f
Adithyaraj <116852652+AdithyaRRb@users.noreply.github.com>
2023-06-07 01:22:00 -0700

    Update validate.py

--- dev/validate/validate.py
+++ dev/validate/validate.py
@@ -1,14 +1,16 @@
 import re
 
-def valid(reading):
-  regcodes=['ap', 'ar', 'as', 'br', 'cg', 'ga', 'gj', 'hr', 'hp', 'jk', 'jh', 'ka', 'kl', 'mp', 'mh', 'mn', 'ml', 'mz', 'nl', 'od', 'pb', 'rj', 'sk', 'tn', 'tr', 'up', 'uk', 'wb', 'ts', 'an', 'ch', 'dn', 'dd', 'ld', 'dl', 'py']
-  for i in regcodes:
-    plate = i + r'\s*\d{2}\s*[a-z]{1-2}\s*\d{1-4}'
-    plate=re.compile(plate)
-    matchfound=re.fullmatch(plate, reading.lower())
-    if matchfound:
-      return matchfound
+def validate(reading):
+    regcodes=['ap', 'ar', 'as', 'br', 'cg', 'ga', 'gj', 'hr', 'hp', 'jk', 'jh', 'ka', 'kl', 'mp', 'mh', 'mn', 'ml', 'mz', 'nl', 'od', 'pb', 'rj', 'sk', 'tn', 'tr', 'up', 'uk', 'wb', 'ts', 'an', 'ch', 'dn', 'dd', 'ld', 'dl', 'py']
+    reading=reading.upper()
+    reading = re.sub(r"[ \n\-\.]", "", reading)
+    for i in regcodes:
+        plate = i + r'\s*\d{1,2}\s*[a-z]{1,2}\s*\d{1,4}'
+        plate=re.compile(plate)
+        matchfound=re.fullmatch(plate, reading.lower())
+        if matchfound:
+            return reading
 
-test = "py34 nh 3209"
+test = "KL 01-Bk \n1001"
+print(validate(test))
 
-print(valid(test))
