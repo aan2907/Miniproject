e7dc9507786d1fc49b1fabc6e7c2ee1364abd974
Ahad Anzar <88426463+ahadanzar@users.noreply.github.com>
2023-06-07 03:15:10 -0700

    Update paddleoc.py

--- dev/ocr/paddleoc.py
+++ dev/ocr/paddleoc.py
@@ -1,15 +1,7 @@
 from paddleocr import PaddleOCR
-import numpy as np
-import cv2, os
+import cv2
 import warnings
 warnings.filterwarnings('ignore')
-
-testpath=r'E:\Mess\Queo\main\results\ocrresults'
-
-def ocr(img_path):
-    ocr = PaddleOCR(lang='en', show_log=False)
-    res=ocr.ocr(img_path)[0]
-    return [[i[0], i[1][0]] for i in res]
 
 def display(img, window):
     y, x= img.shape
@@ -35,32 +27,9 @@ def preprocess(image):
     #display(image, 'Preprocessed')
     return image
 
-
-def visualise(img_path, results):
+def runOcr(img_path):
     img = cv2.imread(img_path)
-    y, x, c = img.shape
-    for bbox in results:
-        cv2.polylines(img, [np.array(bbox[0], np.int32)], True, (0,255,0), thickness=3)
-        #cv2.putText(img, text, (tl[0], tl[1] - 10),
-        #    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-    if y>1000:
-        x=int(1000*x/y)
-        y=1000
-    img = cv2.resize(img, (x, y), interpolation = cv2.INTER_AREA)
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
-
-def runOcr(img_path, testing=False):
-    img = cv2.imread(img_path)
-    if testing:
-        image=img_path.split('\\')[-1][:-4]
-        try:
-            with open(os.path.join(testpath, image + '.txt'), 'r') as res:
-                wordlist = eval(res.read())
-                return [list(a) for a in wordlist], img.shape
-        except FileNotFoundError:
-            pass
     img = preprocess(img)
-    words = ocr(img_path)
-    return words, img.shape
-
+    ocr = PaddleOCR(lang='en', show_log=False)
+    words=ocr.ocr(img_path)[0]
+    return words
