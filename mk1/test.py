0d8e5f9d2c34fcdce5172ef81119496558a272b1
Ahad Anzar <88426463+ahadanzar@users.noreply.github.com>
2023-06-25 13:33:14 +0530

    Delete mk1 directory

--- mk1/test.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-db='vehicles.db'
-connect= sqlite3.connect(db) 
-cur= connect.cursor()
-
-cur.execute("CREATE TABLE CACHE(REGNO TEXT PRIMARY KEY, TIME DATETIME)")
-connect.commit()
